6b75285f37f2387168edc29017518b695a2dd8df
Dustin Godbay <godbayd@aol.com>
2021-06-21 14:41:44 -0400

    added ui.js to consilidate ui programming. Some empty starter props/methods are added to UI class.

---
+++ dist/js/ui.js
@@ -0,0 +1,18 @@
+const UI = function(playBtn, resetBtn, countEl) {
+    this.playBtn = this.playBtn
+    this.resetBtn = this.resetBtn
+    this.countEl = this.countEl
+}
+
+
+UI.prototype.playBtnClickHandler = function(e) {
+    
+}
+
+UI.prototype.resetBtnClickHandler = function(e) {
+    
+}
+
+UI.prototype._bindListener = function(e) {
+    
+}
